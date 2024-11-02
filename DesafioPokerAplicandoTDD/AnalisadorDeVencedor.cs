e572afa760b124d283716b8ffd777b839d4e3c2c
Matheus Gomes <math.s.gomes@gmail.com>
2024-11-01 23:51:49 -0300

    feat: inicializa analisador de vencedor controller

--- DesafioPokerAplicandoTDD/AnalisadorDeVencedor.cs
+++ DesafioPokerAplicandoTDD/AnalisadorDeVencedor.cs
@@ -1,82 +1,23 @@
+using Moq;
+
 namespace DesafioPokerAplicandoTDD;
 
+// Essa classe se tornou uma controler, porque o objetivo dela agora é controlar as outras classes de serviço
 public class AnalisadorDeVencedor
 {
-    public string Analisar(List<string> cartasDoPrimeiroJogador, List<string> cartasDoSegundoJogador)
+    private readonly IAnalisadorDeVencedorComMaiorCarta _analisadorDeVencedorComMaiorCarta;
+    private readonly IAnalisadorDeVencedorComParDeCartas _analisadorDeVencedorComParDeCartas;
+
+    public AnalisadorDeVencedor(
+        IAnalisadorDeVencedorComMaiorCarta analisadorDeVencedorComMaiorCarta,
+        IAnalisadorDeVencedorComParDeCartas analisadorDeVencedorComParDeCartas)
     {
-        var parDeCartasDoPrimeiroJogador = cartasDoPrimeiroJogador
-            .Select(carta => ConverterParaValorDaCarta(carta))
-            .GroupBy(valorDaCarta => valorDaCarta)
-            .Where(grupo => grupo.Count() > 1);
-
-        var parDeCartasDoSegundoJogador = cartasDoSegundoJogador
-            .Select(carta => ConverterParaValorDaCarta(carta))
-            .GroupBy(valorDaCarta => valorDaCarta)
-            .Where(grupo => grupo.Count() > 1);
-
-        if (parDeCartasDoPrimeiroJogador != null && parDeCartasDoPrimeiroJogador.Any() &&
-            parDeCartasDoSegundoJogador != null && parDeCartasDoSegundoJogador.Any())
-        {
-            var maiorParDeCartasPrimeiroJogador = parDeCartasDoPrimeiroJogador
-                .Select(valor => valor.Key).OrderBy(valor => valor).Max();
-
-            var maiorParDeCartasSegundoJogador = parDeCartasDoSegundoJogador
-                .Select(valor => valor.Key).OrderBy(valor => valor).Max();
-
-            if (maiorParDeCartasPrimeiroJogador > maiorParDeCartasSegundoJogador)
-                return "Primeiro Jogador";
-            else if (maiorParDeCartasSegundoJogador > maiorParDeCartasPrimeiroJogador)
-                return "Segundo Jogador";
-        }
-
-        else if (parDeCartasDoPrimeiroJogador != null && parDeCartasDoPrimeiroJogador.Any())
-            return "Primeiro Jogador";
-
-        else if (parDeCartasDoSegundoJogador != null && parDeCartasDoSegundoJogador.Any())
-            return "Segundo Jogador";
-
-        var maiorCartaDoPrimeiroJogador = cartasDoPrimeiroJogador
-            .Select(carta => ConverterParaValorDaCarta(carta))
-            .OrderBy(valorDaCarta => valorDaCarta)
-            .Max();
-
-        var maiorCartaDoSegundoJogador = cartasDoSegundoJogador
-            .Select(carta => ConverterParaValorDaCarta(carta))
-            .OrderBy(valorDaCarta => valorDaCarta)
-            .Max();
-
-        return (maiorCartaDoPrimeiroJogador > maiorCartaDoSegundoJogador)
-            ? "Primeiro Jogador"
-            : "Segundo Jogador";
+        _analisadorDeVencedorComMaiorCarta = analisadorDeVencedorComMaiorCarta;
+        _analisadorDeVencedorComParDeCartas = analisadorDeVencedorComParDeCartas;
     }
 
-    // OBS: Métodos private não são testados diretamente (são testados indiretamente pelos métodos public)
-    private int ConverterParaValorDaCarta(string carta)
+    public string Analisar(List<string> cartasDoPrimeiroJogador, List<string> cartasDoSegundoJogador)
     {
-        var valorDaCarta = carta.Substring(0, carta.Length - 1);
-
-        if (!int.TryParse(valorDaCarta, out var valor))
-        {
-            switch (valorDaCarta)
-            {
-                case "V":
-                    valor = 11;
-                    break;
-                case "D":
-                    valor = 12;
-                    break;
-                case "R":
-                    valor = 13;
-                    break;
-                case "A":
-                    valor = 14;
-                    break;
-                default:
-                    valor = 0;
-                    break;
-            }
-        }
-
-        return valor;
+        return "Implementar";
     }
 }
--- DesafioPokerAplicandoTDD/AnalisadorDeVencedorTest.cs
+++ DesafioPokerAplicandoTDD/AnalisadorDeVencedorTest.cs
@@ -1,3 +1,5 @@
+using Moq;
+
 namespace DesafioPokerAplicandoTDD;
 
 /*
@@ -15,61 +17,20 @@ namespace DesafioPokerAplicandoTDD;
  */
 public class AnalisadorDeVencedorTest
 {
+    private readonly Mock<IAnalisadorDeVencedorComMaiorCarta> _analisadorDeVencedorComMaiorCarta;
     private readonly AnalisadorDeVencedor _analisador;
+    private readonly Mock<IAnalisadorDeVencedorComParDeCartas> _analisadorDeVencedorComParDeCartas;
 
     public AnalisadorDeVencedorTest()
     {
-        _analisador = new AnalisadorDeVencedor();
+        _analisadorDeVencedorComMaiorCarta = new Mock<IAnalisadorDeVencedorComMaiorCarta>();
+        _analisadorDeVencedorComParDeCartas = new Mock<IAnalisadorDeVencedorComParDeCartas>();
+        _analisador = new AnalisadorDeVencedor(
+            _analisadorDeVencedorComMaiorCarta.Object,
+            _analisadorDeVencedorComParDeCartas.Object);
     }
 
-    [Theory]
-    [InlineData("2O,4C,3P,6C,7C", "3O,5C,2E,9C,7P", "Segundo Jogador")]
-    [InlineData( "3O,5C,2E,9C,7P", "2O,4C,3P,6C,7C", "Primeiro Jogador")]
-    [InlineData( "3O,5C,2E,9C,7P", "2O,4C,3P,6C,10E", "Segundo Jogador")]
-    [InlineData( "3O,5C,2E,9C,VP", "2O,4C,3P,6C,AE", "Segundo Jogador")]
-    public void DeveAnalisarVencedorQuandoTiverMaiorCarta(
-        string cartasDoPrimeiroJogadorString,
-        string cartasDoSegundoJogadorString,
-        string vencedorEsperado)
-    {
-        var cartasDoPrimeiroJogador = cartasDoPrimeiroJogadorString.Split(",").ToList();
-        var cartasDoSegundoJogador = cartasDoSegundoJogadorString.Split(",").ToList();
-
-        var vencedor = _analisador.Analisar(cartasDoPrimeiroJogador, cartasDoSegundoJogador);
-
-        Assert.Equal(vencedorEsperado, vencedor);
-    }
-
-    [Theory]
-    [InlineData("2O,2C,3P,6C,7C", "3O,5C,2E,9C,7P", "Primeiro Jogador")]
-    [InlineData("3O,5C,2E,9C,7P", "2O,2C,3P,6C,7C", "Segundo Jogador")]
-    [InlineData("2O,2C,3P,6C,7C", "DO,DC,2E,9C,7P", "Segundo Jogador")]
-    [InlineData("DO,DC,2E,9C,7P", "2O,2C,3P,6C,7C", "Primeiro Jogador")]
-    public void DeveAnalisarVencedorQuandoTiverUmParDeCartasDoMesmoValor(
-        string cartasDoPrimeiroJogadorString,
-        string cartasDoSegundoJogadorString,
-        string vencedorEsperado)
-    {
-        var cartasDoPrimeiroJogador = cartasDoPrimeiroJogadorString.Split(",").ToList();
-        var cartasDoSegundoJogador = cartasDoSegundoJogadorString.Split(",").ToList();
-
-        var vencedor = _analisador.Analisar(cartasDoPrimeiroJogador, cartasDoSegundoJogador);
-
-        Assert.Equal(vencedorEsperado, vencedor);
-    }
-
-    [Theory]
-    [InlineData("3O,5C,RE,AC,RP", "RO,4C,3P,RC,7C", "Primeiro Jogador")]
-    [InlineData("RO,4C,3P,RC,7C", "3O,5C,RE,AC,RP", "Segundo Jogador")]
-    public void DeveAnalisarVencedorQuandoDoisJogadoresEstaoEmpatadosEmParEhVencedorOQueTemAMaiorCarta(
-        string cartasDoPrimeiroJogadorString,
-        string cartasDoSegundoJogadorString,
-        string vencedorEsperado)
-    {
-        var cartasDoPrimeiroJogador = cartasDoPrimeiroJogadorString.Split(",").ToList();
-        var cartasDoSegundoJogador = cartasDoSegundoJogadorString.Split(",").ToList();
-        var vencedor = _analisador.Analisar(cartasDoPrimeiroJogador, cartasDoSegundoJogador);
-
-        Assert.Equal(vencedorEsperado, vencedor);
-    }
+    [Fact]
+    public void DeveAnalisarVencedorQueTemMaiorCarta()
+    {}
 }
